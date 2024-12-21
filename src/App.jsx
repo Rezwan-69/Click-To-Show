3b7ddd7e540226019861713bf8260f467716dc99
rezwan-mahmud-utso <itz.rezwan69@gmail.com>
2024-12-21 15:38:33 +0600

    ami

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import { FaMinusCircle } from "react-icons/fa";
+
+function App() {
+  let [show, setShow] = useState(false);
+  let handelshow = () => {
+    setShow(!show);
+  };
+
+  return (
+    <>
+      <h1 onClick={handelshow}>
+        Click me {show == true ? <FaMinusCircle /> : <FaPlus />}
+      </h1>
+
+      {/* {show == true ? "ami tomake" : ""} */}
+      {show && (
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores,
+          tenetur aliquid aut qui consequuntur optio dolores corporis iste cum
+          quis assumenda rerum mollitia quia tempora repellendus, numquam
+          eligendi quo sequi explicabo. At nemo saepe quasi eos reiciendis,
+          error quaerat explicabo ullam expedita quas provident molestias esse
+          soluta iusto, doloremque culpa.
+        </p>
+      )}
+    </>
+  );
+}
+
+export default App;
